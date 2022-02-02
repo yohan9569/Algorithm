6a0aaea1e75b5e21625005bb702b34a717d742e3
Yohan Park <88149844+yohan9569@users.noreply.github.com>
2022-02-02 22:52:34 +0900

    Create 7568.py

---
+++ BOJ/7568.py
@@ -0,0 +1,18 @@
+# 7568번 덩치
+
+xy=[]
+ans=[]
+
+for _ in range(int(input())):
+    xy.append(tuple(int(i) for i in input().split()))
+    
+for i in xy:
+    cnt=0
+    
+    for j in xy:
+        if i[0]<j[0] and i[1]<j[1]: 
+          cnt+=1
+    
+    ans.append(cnt+1)
+    
+print(' '.join(str(i) for i in ans))
