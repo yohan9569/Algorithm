bf19339811de1d1aecae13b10c03ceee5deb3e4b
Yohan Park <88149844+yohan9569@users.noreply.github.com>
2022-02-21 16:00:08 +0900

    Create 11729.py

---
+++ BOJ/11729.py
@@ -0,0 +1,21 @@
+# 백준 11729 재귀 - 하노이 탑 이동 순서
+
+def hanoi(n, from_, to_):
+    if n==1:
+        print(from_, to_, sep=' ')
+        return
+      
+    hanoi(n-1, from_, 6-from_-to_)
+    print(from_, to_, sep=' ')
+    hanoi(n-1, 6-from_-to_, to_)
+    return
+  
+def aaa(n):
+    if n==1:
+        return 1
+      
+    return 1 + 2*aaa(n-1)
+  
+n = int(input())
+print(aaa(n))
+hanoi(n, 1, 3)
