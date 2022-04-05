1ad06da203ed7438c4367bd702034b212f4a6fc2
Yohan Park <88149844+yohan9569@users.noreply.github.com>
2022-04-06 01:56:25 +0900

    Create 11653.py

---
+++ BOJ/11653.py
@@ -0,0 +1,19 @@
+# boj 백준 수학2 11653번 소인수분해
+
+a=[]
+
+def sol(a,n):
+    for i in range(1,n+1):
+        if i==1:
+            continue
+            
+        elif n%i==0:
+            n=n//i
+            a.append(i)
+            break
+    if n>1: 
+        sol(a,n)
+        
+    return a
+  
+print(*sol(a, int(input())), sep='\n')
