5baaccf6cae90b273cc9e988a410e8831437dc59
Yohan Park <88149844+yohan9569@users.noreply.github.com>
2022-04-10 20:19:32 +0900

    Create 1110.py

---
+++ BOJ/1110.py
@@ -0,0 +1,16 @@
+# boj 백준 while 1110번 더하기 사이클
+
+n=int(input())
+num=n
+
+cycle=0
+
+while 1:
+    a,b=divmod(n,10)
+    c=(a+b)%10
+    n=(10*b)+c
+    cycle+=1
+    if n==num:
+        break
+      
+print(cycle)
