0d02dc2ecc392539df27a77c586a27815efd0e0e
Yohan Park <88149844+yohan9569@users.noreply.github.com>
2022-04-14 23:36:47 +0900

    Create 9020.py

---
+++ BOJ/9020.py
@@ -0,0 +1,16 @@
+# boj 백준 수학2 9020번 골든바흐의 추측
+
+prime=set(range(2,10001))
+
+for i in range(2,101):
+    if i in prime:
+        prime-=set(range(2*i,10001,i))
+        
+prime=sorted(list(prime))
+
+# for a in [*map(int,[*open(0)][1:])]:
+for a in [3,8,10,16]:
+    for j in range(a//2,1,-1): # prime element로 돌리느라 고생
+        if j in prime and a-j in prime:
+            print(j,a-j)
+            break
