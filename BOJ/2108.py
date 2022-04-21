47a36676db1c10e18fecc307dd8f5451fdac5def
Yohan Park <88149844+yohan9569@users.noreply.github.com>
2022-04-22 01:21:10 +0900

    Create 2108.py

---
+++ BOJ/2108.py
@@ -0,0 +1,18 @@
+# boj 백준 정렬 2108번 통계학
+import sys
+from collections import Counter
+
+n = int(sys.stdin.readline())
+nums = sorted([int(sys.stdin.readline()) for _ in range(n)])
+# 중앙값 때만 sort하면, 최빈값 때 제대로 정렬되어있지 않아서 문제되는 것 같다.
+
+print(round(sum(nums)/n))
+print(nums[n//2])
+
+mod = Counter(nums).most_common()
+if len(mod)>1 and mod[0][1] == mod[1][1]: 
+    print(mod[1][0])
+else: 
+    print(mod[0][0])
+
+print(nums[-1]-nums[0])
