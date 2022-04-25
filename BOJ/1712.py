bc32e4a3afdf725e991b78c3740879b1229a80f5
Yohan Park <88149844+yohan9569@users.noreply.github.com>
2022-04-25 20:41:53 +0900

    Create 1712.py

---
+++ BOJ/1712.py
@@ -0,0 +1,13 @@
+# boj 백준 1712번 손익분기점
+
+a,b,c = map(int,input().split())
+
+if c==b: 
+    print(-1)
+    
+else:
+    n = int(a/(c-b))
+    if n<0: 
+        print(-1)
+    else: 
+        print(n+1)
