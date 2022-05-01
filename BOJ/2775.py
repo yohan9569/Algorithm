1e8a815d9c92118752a244621ec6c78bea06588c
Yohan Park <88149844+yohan9569@users.noreply.github.com>
2022-05-01 19:53:59 +0900

    Create 2775.py

---
+++ BOJ/2775.py
@@ -0,0 +1,12 @@
+# boj 백준 수학 2775번 부녀회장이 될테야
+
+for _ in range(int(input())):
+    k = int(input())
+    n = int(input())
+    a = [i for i in range(1,n+1)]
+    
+    while k>1:
+        a = [sum(a[:i+1]) for i in range(n)]
+        k -= 1
+        
+    print(sum(a))
