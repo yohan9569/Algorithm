c628db027d2647be19a878038106bbdbbfa918b7
Yohan Park <88149844+yohan9569@users.noreply.github.com>
2022-05-25 00:21:27 +0900

    Create 13305.py

---
+++ BOJ/13305.py
@@ -0,0 +1,19 @@
+# boj 그리디 13305번 주유소
+
+
+n = int(input())
+dis = [*map(int, input().split())]
+price = [*map(int,input().split())]
+
+ans = 0
+greed = price[0]
+
+for i,v in enumerate(dis):
+    ans += (v*greed)
+    next = price[i+1]
+    
+    if greed>next: 
+        greed = next
+
+        
+print(ans)
