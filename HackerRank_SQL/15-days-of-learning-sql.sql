e4bb71a063014632176f942f28a4f1e57a553bd2
Yohan Park <88149844+yohan9569@users.noreply.github.com>
2022-10-04 19:39:34 +0900

    Solved 15-days-of-learning-sql.sql

--- HackerRank_SQL/15-days-of-learning-sql.sql
+++ HackerRank_SQL/15-days-of-learning-sql.sql
@@ -1,17 +1,34 @@
--- 현재 계획 날마다 제출한 인원수 & 날마다 가장 제출 많이한 사람 & 이름 join
+-- 15 Days of Learning SQL
 
--- -- total number of unique hackers
--- SELECT S.submission_date, COUNT(DISTINCT S.hacker_id)
--- FROM Submissions S
--- GROUP BY S.submission_date
--- ORDER BY S.submission_date
 
--- 날마다 가장 제출 많이한 사람 뽑기
-SELECT submission_date, hacker_id
-FROM(SELECT submission_date, COUNT(hacker_id) CNT, hacker_id
-    FROM Submissions
+SELECT totals.submission_date, totals.total, maximums.maximum, H.name
+FROM (
+    -- total number of unique hackers
+    SELECT submission_date, COUNT(DISTINCT hacker_id) total
+    FROM Submissions S1
+    WHERE (submission_date - DATE('2016-03-01')) = (
+        SELECT COUNT(DISTINCT submission_date)
+        FROM Submissions S1_1
+        WHERE S1.submission_date > S1_1.submission_date
+            AND S1.hacker_id = S1_1.hacker_id
+        )
+    GROUP BY submission_date
+) totals
+JOIN(
+    -- the hacker who made maximum number of submissions
+    SELECT submission_date, hacker_id maximum
+    FROM Submissions S2
+    WHERE hacker_id = (
+        SELECT hacker_id
+        FROM Submissions S2_1
+        WHERE S2.submission_date = S2_1.submission_date
+        GROUP BY hacker_id
+        ORDER BY COUNT(submission_id) DESC, hacker_id
+        LIMIT 1
+        )
     GROUP BY submission_date, hacker_id
-    ORDER BY submission_date) CT
-GROUP BY submission_date
-WHERE CNT = MAX(CNT) -- 여기가 안 풀리는 중
-
+) maximums
+ON totals.submission_date = maximums.submission_date
+JOIN Hackers H
+ON maximums.maximum = H.hacker_id
+ORDER BY totals.submission_date;
