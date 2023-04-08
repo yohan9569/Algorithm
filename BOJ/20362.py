c168db3c195804554728f476cb2912e51be2c836
Yohan Park <88149844+yohan9569@users.noreply.github.com>
2023-04-09 00:11:29 +0900

    구현 / 브론즈 1 / 유니대전 퀴즈쇼

---
+++ BOJ/20362.py
@@ -0,0 +1,16 @@
+# 구현 / 브론즈 1 / 유니대전 퀴즈쇼
+
+
+from collections import defaultdict
+
+n,s = input().split()
+d = defaultdict(int)
+ans = 0
+
+for _ in range(int(n)):
+    x,y = input().split()
+    if x==s:
+        break
+    d[y]+=1
+
+print(d[y])
