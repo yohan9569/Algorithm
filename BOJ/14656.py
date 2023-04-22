98e099c880033074f9086027122af1942223a2ce
Yohan Park <88149844+yohan9569@users.noreply.github.com>
2023-04-22 23:09:12 +0900

    구현 / 브론즈 3 / 조교는 새디스트야!!

--- BOJ/14656.py
+++ BOJ/14656.py
@@ -3,3 +3,8 @@
 n = int(input())
 l = map(int, input().split())
 print(sum(i!=j for i,j in zip(l, range(1,n+1))))
+
+
+
+# another way - str으로 비교
+i=0;print(sum(str(i:=i+1)!=j for j in[*open(0)][1].split()))
