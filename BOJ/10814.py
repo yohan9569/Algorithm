8eb97b93d4312c0abdaa5057f7431bf878b7c867
Yohan Park <88149844+yohan9569@users.noreply.github.com>
2023-05-16 02:21:17 +0900

    정렬 / 실버 5 / 나이순 정렬

--- BOJ/10814.py
+++ BOJ/10814.py
@@ -1,4 +1,4 @@
-# 백준 정렬 10814번 나이순 정렬
+# 정렬 / 실버 5 / 나이순 정렬
 
 from operator import itemgetter
 
@@ -7,6 +7,6 @@ member = [(int(age), name) for age, name in (row.split() for row in [*open(0)][1
 for m in sorted(member, key=itemgetter(0)): 
     print(m[0], m[1])
 
-    
-# 우수 코드
+
+# another way
 print(*sorted([*open(0)][1:],key=lambda a:int(a.split()[0])),sep='')
