2a22db7ffed6ad511c80831379cc7760f987948a
Yohan Park <88149844+yohan9569@users.noreply.github.com>
2023-06-26 00:10:17 +0900

    수학 / 브론즈 1 / 평균

--- BOJ/1546.py
+++ BOJ/1546.py
@@ -1,4 +1,5 @@
-# 1546
-N=int(input())
-a=[int(i) for i in input().split()]
-print(sum(i/max(a)*100 for i in a)/N)
+# 수학 / 브론즈 1 / 평균
+
+N = int(input())
+a = [int(i) for i in input().split()]
+print( sum(i/max(a) * 100 for i in a) / N )
