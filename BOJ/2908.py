9ab54338cdb855ac1654be5eb851d141dff11f85
Yohan Park <88149844+yohan9569@users.noreply.github.com>
2023-08-05 21:46:51 +0900

    구현 / 브론즈 2 / 상수

--- BOJ/2908.py
+++ BOJ/2908.py
@@ -1,6 +1,11 @@
-# 2908번 숫자를 뒤집어서 비교
+# 구현 / 브론즈 2 / 상수
+# 숫자를 뒤집어서 비교
+
+
 A, B = (int(''.join(list(reversed(i)))) for i in input().split())
 print(max(A,B))
 
-# 더 쉬운 # Using extended slice syntax
+
+
+# 더 쉬운 방법 # Using extended slice syntax
 print(max(input()[::-1].split()))
