ee1f10e190796e2674a39fe9fa48eafb3f01bddf
Yohan Park <88149844+yohan9569@users.noreply.github.com>
2023-08-17 23:24:19 +0900

    수학 / 실버 2 / 골든바흐의 추측

--- BOJ/9020.py
+++ BOJ/9020.py
@@ -1,11 +1,12 @@
-# boj 백준 수학2 9020번 골든바흐의 추측
+# 수학 / 실버 2 / 골든바흐의 추측
 
-prime=set(range(2,10001))
+
+prime = set(range(2,10001))
 
 for i in range(2,101):
     if i in prime:
-        prime-=set(range(2*i,10001,i))
-        
+        prime -= set(range(2*i,10001,i))
+
 prime=sorted(list(prime))
 
 # for a in [*map(int,[*open(0)][1:])]:
