6fdc826fdad23190f2e07d2897288050572ca6c9
Yohan Park <88149844+yohan9569@users.noreply.github.com>
2024-05-15 14:30:04 +0900

    [Silver IV] Title: 주몽, Time: 468 ms, Memory: 35120 KB -BaekjoonHub

--- 백준/Silver/1940. 주몽/주몽.java
+++ 백준/Silver/1940. 주몽/주몽.java
@@ -1,24 +1,21 @@
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt(), M = sc.nextInt(), cnt = 0, arr[] = new int[N];
+  public static void main(String[] args){
+    Scanner sc = new Scanner(System.in);
+    int N = sc.nextInt(), M = sc.nextInt(), cnt = 0, arr[]=new int[N];
 
-        for (int i=0; i<N; i++) {
-        	arr[i] = sc.nextInt();
-        }
-        Arrays.sort(arr);
+    for (int i=0; i<N; arr[i++] = sc.nextInt());
+    Arrays.sort(arr);
 
-        int i = 0, j = N-1;
-        while (i<j) {
-		        if (arr[i] + arr[j] < M) i++;
-		        else if (arr[i] + arr[j] > M) j--;
-		        else {
-				        cnt++; i++; j--;
-		        }
-        }
-
-        System.out.print(cnt);
+    int i = 0, j = N-1;
+    while (i<j) {
+	  if (arr[i] + arr[j] < M) i++;
+	  else if (arr[i] + arr[j] > M) j--;
+	  else {
+	    cnt++; i++; j--;
+	  }
     }
+    System.out.print(cnt);
+  }
 }
