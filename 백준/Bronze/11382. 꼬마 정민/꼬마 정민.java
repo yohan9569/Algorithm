c3633b2b1ccdc46c9f00c1748c2c340090b4ba17
Yohan Park <88149844+yohan9569@users.noreply.github.com>
2024-05-17 22:06:39 +0900

    [Bronze V] Title: 꼬마 정민, Time: 212 ms, Memory: 17680 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
@@ -0,0 +1,9 @@
+import java.util.*;
+class Main {
+  public static void main(String[] args) {
+    var sc = new Scanner(System.in);
+    long a = 0, i = 3;
+    while (i-->0) a += sc.nextLong();
+    System.out.print(a);
+  }
+}
