6247c98d2e1b968897d18242bdb729b739dcf544
Yohan Park <88149844+yohan9569@users.noreply.github.com>
2024-05-19 22:10:07 +0900

    [Silver II] Title: 스택 수열, Time: 996 ms, Memory: 106552 KB -BaekjoonHub

--- 백준/Silver/1874. 스택 수열/스택 수열.java
+++ 백준/Silver/1874. 스택 수열/스택 수열.java
@@ -7,29 +7,22 @@ class Main {
         for (int i=0; i<n; arr[i++]=sc.nextInt());
         var stack = new Stack<Integer>();
         var ans = new StringBuffer();
-        var flag = true;
 
         for (int i=0; i<arr.length; i++) {
             int elem = arr[i];
-            
-            if (next <= elem) {
-                while (next <= elem) {
-                    stack.push(next++);
-                    ans.append("+\n");
-                }
-                stack.pop();
-                ans.append("-\n");
-            } else {
-                int top = stack.pop();
-                if (top == elem) ans.append("-\n");
-                else {
-                    System.out.print("NO");
-                    flag = false;
-                    break;
-                }
+
+            while (next <= elem) {
+                stack.push(next++);
+                ans.append("+\n");
+            }
+            int top = stack.pop();
+            if (top == elem) ans.append("-\n");
+            else {
+                System.out.print("NO");
+                return;
             }
         }
 
-        if (flag) System.out.print(ans.toString());
+        System.out.print(ans.toString());
     }
 }
