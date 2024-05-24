8fdbb3a074c98ffeab3319a5af6e0559fb49ffda
Yohan Park <88149844+yohan9569@users.noreply.github.com>
2024-05-24 14:15:07 +0900

    [Bronze V] Title: A-B, Time: 208 ms, Memory: 17724 KB -BaekjoonHub

---
+++ 백준/Bronze/1001. A－B/A－B.java
@@ -0,0 +1,7 @@
+import java.util.*;
+class Main {
+    public static void main(String[] args) {
+        var sc = new Scanner(System.in);
+        System.out.print(sc.nextInt()-sc.nextInt());
+    }
+}
