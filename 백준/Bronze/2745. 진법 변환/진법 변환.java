15dbf6fca08624a108a32bc715b0aa834adbb23a
Yohan Park <88149844+yohan9569@users.noreply.github.com>
2024-05-30 08:58:24 +0900

    [Bronze II] Title: 진법 변환, Time: 208 ms, Memory: 17744 KB -BaekjoonHub

---
+++ 백준/Bronze/2745. 진법 변환/진법 변환.java
@@ -0,0 +1,9 @@
+import java.util.*;
+class Main {
+    public static void main(String[] args) {
+        var sc = new Scanner(System.in);
+        String n = sc.next();
+        int b = sc.nextInt();
+        System.out.print(Integer.parseInt(n, b));
+    }
+}
