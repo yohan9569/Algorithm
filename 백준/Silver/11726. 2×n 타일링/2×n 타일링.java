6d0fe58e8fd2658555bffee47274b94825399b72
Yohan Park <88149844+yohan9569@users.noreply.github.com>
2024-07-11 14:02:10 +0900

    [Silver III] Title: 2×n 타일링, Time: 176 ms, Memory: 17740 KB -BaekjoonHub

--- 백준/Silver/11726. 2×n 타일링/2×n 타일링.java
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.java
@@ -4,16 +4,13 @@ import java.util.Scanner;
 public class Main {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
-
-    final long mod = 10007;
     int n = sc.nextInt();
+    sc.close();
     long d[] = new long[1001];
     d[1] = 1;
     d[2] = 2;
-    for (int i = 3; i <= n; i++) {
-      d[i] = (d[i - 1] + d[i - 2]) % mod;
-    }
-
+    for (int i = 3; i <= n; i++)
+      d[i] = (d[i - 1] + d[i - 2]) % 10007;
     System.out.println(d[n]);
   }
 }
