598a2f52aebe67b030c94fc5d686a177644436c7
Yohan Park <88149844+yohan9569@users.noreply.github.com>
2024-07-21 23:19:17 +0900

    [Bronze I] Title: 소인수분해, Time: 904 ms, Memory: 29200 KB -BaekjoonHub

---
+++ 백준/Bronze/11653. 소인수분해/소인수분해.py
@@ -0,0 +1,11 @@
+a=[]
+def sol(a,n):
+    for i in range(1,n+1):
+        if i==1:continue
+        elif n%i==0:
+            n=n//i
+            a.append(i)
+            break
+    if n>1: sol(a,n)
+    return a
+print(*sol(a, int(input())), sep='\n')
