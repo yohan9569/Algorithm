b6bef7550f89b35a63911e0b096acb282523d66d
Yohan Park <88149844+yohan9569@users.noreply.github.com>
2024-07-23 13:54:23 +0900

    [Bronze II] Title: 소수, Time: 680 ms, Memory: 29200 KB -BaekjoonHub

---
+++ 백준/Bronze/2581. 소수/소수.py
@@ -0,0 +1,14 @@
+s=set()
+for i in range(int(input()), int(input())+1):
+    if i==1: continue
+    a=0
+    for j in range(2,i):
+        if i%j==0: 
+            a=1
+            break
+    if a:continue
+    s.add(i)
+if len(s)==0: print(-1)
+else: 
+    print(sum(s))
+    print(min(s))
