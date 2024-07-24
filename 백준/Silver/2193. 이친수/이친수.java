e6d7f3c388f50f9d696571aa55f44982ef6d2826
Yohan Park <88149844+yohan9569@users.noreply.github.com>
2024-07-24 17:46:23 +0900

    [Silver III] Title: 이친수, Time: 168 ms, Memory: 17692 KB -BaekjoonHub

---
+++ 백준/Silver/2193. 이친수/이친수.java
@@ -0,0 +1,20 @@
+// Dynamic Programming Alogrithm
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    long[][] d = new long[n + 1][2]; // [각 자리수][0인 경우, 1인 경우]
+
+    d[1][0] = 0;
+    d[1][1] = 1;
+
+    for (int i = 2; i <= n; i++) {
+      d[i][0] = d[i - 1][0] + d[i - 1][1];
+      d[i][1] = d[i - 1][0];
+    }
+
+    System.out.println(d[n][0] + d[n][1]);
+  }
+}
