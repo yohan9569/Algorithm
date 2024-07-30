f3f60d92d05cb9f719a0ed077ef9206c8ddc7049
Yohan Park <88149844+yohan9569@users.noreply.github.com>
2024-07-31 01:27:21 +0900

    [Bronze III] Title: 약수 구하기, Time: 208 ms, Memory: 17740 KB -BaekjoonHub

---
+++ 백준/Bronze/2501. 약수 구하기/약수 구하기.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt(), k = sc.nextInt(), cnt=0;
+    for (int i = 1; i<=n; i++) {
+      if (n%i==0) cnt++;
+      if (cnt==k) {
+        System.out.println(i);
+        return;
+      }
+    }
+    System.out.println(0);
+  }
+}
