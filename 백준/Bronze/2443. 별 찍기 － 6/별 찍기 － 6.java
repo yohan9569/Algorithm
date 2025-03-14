443a919072e0ac7e3a8cf6d3877cdda9f609cc46
Yohan Park <88149844+yohan9569@users.noreply.github.com>
2025-03-15 01:01:49 +0900

    [Bronze III] Title: 별 찍기 - 6, Time: 176 ms, Memory: 17732 KB -BaekjoonHub

--- 백준/Bronze/2443. 별 찍기 － 6/별 찍기 － 6.java
+++ 백준/Bronze/2443. 별 찍기 － 6/별 찍기 － 6.java
@@ -5,8 +5,8 @@ public class Main {
     Scanner sc = new Scanner(System.in);
     int n = sc.nextInt();
 
-    for (int i = 0; i < n; i++) {
-      System.out.println(" ".repeat(i) + "*".repeat(2 * n - 2 * i - 1));
+    for (int i = n; i > 0; i--) {
+      System.out.println(" ".repeat(n-i) + "*".repeat(2*i-1));
     }
   }
 }
